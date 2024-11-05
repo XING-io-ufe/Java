9485d4e3f86585966aa2713d93ae460b3f89afa2
Odokaa (オドカー) <xingdaiyamondo@gmail.com>
2024-11-05 15:10:05 +0800

    Initial commit

---
+++ Test/src/One.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class One {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        int sum= 0;
+        n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
+}
+}
